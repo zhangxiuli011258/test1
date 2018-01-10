a93883ed13cd23743f8e5758476810e8f1c5d6d1
zhangxiuli011258 <1075744603@qq.com>
2018-01-10 13:07:10 +0800

    哈哈哈试试

---
+++ src/com/example/test/Test.java
@@ -0,0 +1,23 @@
+package com.example.test;
+
+/**
+ * 文件名称： com.example.test.Test.java</br>
+ * 初始作者： zhangxiuli</br>
+ * 创建日期： 2018年1月3日</br>
+ * 功能说明： 这里用一句话描述这个类的作用--此句话需删除 <br/>
+ * =================================================<br/>
+ * 修改记录：<br/>
+ * 修改作者 日期 修改内容<br/>
+ * ================================================<br/>
+ * Copyright (c) 2010-2011 .All rights reserved.<br/>
+ */
+public class Test {
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+		System.out.print("hellow word");
+
+	}
+
+}
